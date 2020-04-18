025defaf98ee35b26dbc93b030af4e1a1a320983
蔡金锋 <jeff.cai@99bill.com>
2020-04-18 15:03:57 +0800

    更新文档

--- .umirc.js
+++ .umirc.js
@@ -1,7 +1,7 @@
 const pkg = require('./package.json');
 // const version = `${pkg.version.split('.')[0]}.x`;
 
-const serverRootDirect = process.env.NODE_ENV === 'production' ? 'https://doly-dev.github.io/rc-hooks/' : '/';
+const serverRootDirect = process.env.NODE_ENV === 'production' ? 'https://doly-dev.github.io/doly-admin-components/' : '/';
 const logo = 'https://www.caijinfeng.com/assets/images/logo-doly@3x.png';
 const favicon = 'https://www.caijinfeng.com/assets/images/doly-touch-icon_48x48.png';
 

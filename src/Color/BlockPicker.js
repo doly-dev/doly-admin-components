f5353bf6b47b8ef08f2a3f3b4fead31088138499
蔡金锋 <jeff.cai@99bill.com>
2020-04-20 21:24:16 +0800

    去掉注释

--- src/Color/BlockPicker.js
+++ src/Color/BlockPicker.js
@@ -1,7 +1,6 @@
 import React, { useCallback, useState } from "react";
 import { Popover } from "antd";
 import { BlockPicker } from "react-color";
-// import useStyles from "./style";
 
 import styles from "./style.less";
 
@@ -13,7 +12,6 @@ export default ({
   colors = ['#D9E3F0', '#F47373', '#697689', '#37D67A', '#2CCCE4', '#555555', '#dce775', '#ff8a65', '#ba68c8']
 }) => {
   const [visible, setVisible] = useState(false);
-  // const classes = useStyles();
 
   const handleChangeComplete = useCallback(color => {
     onChange(color.hex);

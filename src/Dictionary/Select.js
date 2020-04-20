0fc08d06c74748e7b3f141aa43ee15c7cb199239
蔡金锋 <jeff.cai@99bill.com>
2020-04-20 10:57:57 +0800

    去掉Dictionary Select默认value

--- src/Dictionary/Select.js
+++ src/Dictionary/Select.js
@@ -1,14 +1,14 @@
 import React, { useMemo } from "react";
 import { Select } from "antd";
 
-export default ({ data = [], excludeValues = [], value = "", all = true, allValue = "", allName = "全部", ...restProps }) => {
+export default ({ data = [], excludeValues = [], all = true, allValue = "", allName = "全部", ...restProps }) => {
 
   const dataRet = useMemo(() => {
     return data.slice().filter(item => excludeValues.indexOf(item.value) === -1);
   }, [data, excludeValues]);
 
   return (
-    <Select placeholder="请选择" value={value} {...restProps}>
+    <Select placeholder="请选择" {...restProps}>
       {
         all ? <Select.Option value={allValue}>{allName}</Select.Option> : null
       }

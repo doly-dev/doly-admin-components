215076d48ffa478f23da7bf86e2fda5162e191a9
蔡金锋 <jeff.cai@99bill.com>
2020-04-25 22:30:28 +0800

    去掉多余样式

--- src/Color/EditableInput.js
+++ src/Color/EditableInput.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { EditableInput } from "react-color/lib/components/common";
-console.log(EditableInput);
 
 export default (props) => {
   return (
-    <EditableInput label="hex" {...props} />
+    <EditableInput {...props} />
   );
 };
